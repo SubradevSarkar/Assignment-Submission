19028c15c959996801e9b6f1713bd82d95e93dc5
SubradevSarkar <76807055+SubradevSarkar@users.noreply.github.com>
2021-07-08 20:36:25 +0530

    Create Assignment1.js

---
+++ Assignment1.js
@@ -0,0 +1,16 @@
+//Javascript Essentials course(LetsUpgrade)
+
+//Auth - Subradev sarkar
+
+// * * *  Assignment 1 * * * 
+
+// Question 1
+// Print 5 stars in same line . 
+//output - (* * * * *)
+
+s = ''
+
+for (i = 0; i < 5; i++) {
+    s += '* '
+}
+console.log(s);
